d40af7ec2679d5db193f58c99304605b768fe3cf
kenya.sugino <rui13seirui13sei@gmail.com>
2021-02-27 04:29:36 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
